feaf38d04e2cc21e465358c8f5a4e7f837ebfad8
Martin Schorfmann <schorfma@uni-bremen.de>
2020-11-13 23:24:18 +0100

    Commented and refactored code, Localization structure

--- data_science_case_study_exploration.py
+++ data_science_case_study_exploration.py
@@ -6,49 +6,85 @@ import i18n
 from typing import Callable, Dict, Text
 
 
-TRANSLATIONS_PATH = Path("./translations")
+# Definition of Global Variables
 
-TRANSLATION_LANGUAGES = {
+# Path to directory containing the translation files
+TRANSLATION_PATH = Path("./translations")
+
+# Available languages (Language name for language code)
+TRANSLATION_LANGUAGES: Dict[Text, Text] = {
     "en": "English",
     "de": "Deutsch"
 }
 
+# Default language to fall back to
 FALLBACK_LANGUAGE = "en"
 
+# Definition of Functions
+
 def load_translation(
-        translations_path: Path,
-        translation_languages: Dict[Text, Text],
+        translation_path: Path,
         language: Text,
         fallback_language: Text
 ) -> Callable[[Text], Text]:
+    """Loads the chosen translation of the app.
+
+    Args:
+        translation_path:
+            Path to directory containing translation files.
+        language:
+            Language code of chosen language.
+        fallback_language:
+            The default language code to fall back to.
+
+    Returns:
+        The translation function accepting a textual key and returning the
+        localized string.
+    """
+
+    # Default configuration values:
+    #     https://github.com/danhper/python-i18n/blob/master/i18n/config.py
+
+    # Keep localization in memory
     i18n.set("enable_memoization", True)
+
+    # Output error for missing translation keys
     i18n.set("error_on_missing_translation", True)
 
-    i18n.load_path.append(translations_path)
+    # Set the translation path
+    i18n.load_path.append(translation_path)
 
+    # Set the selected language
     i18n.set("locale", language)
+
+    # Set the fallback language
     i18n.set("fallback", fallback_language)
 
     return i18n.t
 
 
+# Streamlit Script
+
+# Language Selection Widget
 LANGUAGE = streamlit.sidebar.selectbox(
     label="🌍 Select Language",
     options=list(TRANSLATION_LANGUAGES.keys()),
     format_func=TRANSLATION_LANGUAGES.get
 )
 
+# Translation Function to get localized Strings
 TRANSLATION = load_translation(
-    TRANSLATIONS_PATH,
-    TRANSLATION_LANGUAGES,
+    TRANSLATION_PATH,
     language=LANGUAGE,
     fallback_language=FALLBACK_LANGUAGE
 )
 
+# Title of Web Application
 streamlit.title(
     TRANSLATION("common.title")
 )
 
+# Hello World Message
 streamlit.write(
     TRANSLATION("common.hello_world")
 )
