f304770e046db66e253889dfed0dda5cac7b8171
Martin Schorfmann <schorfma@uni-bremen.de>
2020-11-13 19:44:22 +0100

    Added multilingual translations

--- data_science_case_study_exploration.py
+++ data_science_case_study_exploration.py
@@ -1,5 +1,54 @@
+from pathlib import Path
+
 import streamlit
+import i18n
 
-streamlit.title("Data Science Basics with Case Study Exploration")
+from typing import Callable, Dict, Text
 
-streamlit.write("Hello World!")
+
+TRANSLATIONS_PATH = Path("./translations")
+
+TRANSLATION_LANGUAGES = {
+    "en": "English",
+    "de": "Deutsch"
+}
+
+FALLBACK_LANGUAGE = "en"
+
+def load_translation(
+        translations_path: Path,
+        translation_languages: Dict[Text, Text],
+        language: Text,
+        fallback_language: Text
+) -> Callable[[Text], Text]:
+    i18n.set("enable_memoization", True)
+    i18n.set("error_on_missing_translation", True)
+
+    i18n.load_path.append(translations_path)
+
+    i18n.set("locale", language)
+    i18n.set("fallback", fallback_language)
+
+    return i18n.t
+
+
+LANGUAGE = streamlit.sidebar.selectbox(
+    label="🌍 Select Language",
+    options=list(TRANSLATION_LANGUAGES.keys()),
+    format_func=TRANSLATION_LANGUAGES.get
+)
+
+TRANSLATION = load_translation(
+    TRANSLATIONS_PATH,
+    TRANSLATION_LANGUAGES,
+    language=LANGUAGE,
+    fallback_language=FALLBACK_LANGUAGE
+)
+
+streamlit.title(
+    TRANSLATION("common.title")
+)
+
+streamlit.write(
+    TRANSLATION("common.hello_world")
+)
